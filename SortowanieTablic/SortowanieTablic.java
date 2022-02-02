bffaf8878b975751ad169adaec14c861bffafbc9
DawidGwazdacz <gwazdaczdawid@wp.pl>
2022-02-02 09:29:25 +0100

    SortowanieTablic

--- SortowanieTablic/SortowanieTablic.java
+++ SortowanieTablic/SortowanieTablic.java
@@ -1,31 +1,36 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class SortowanieTablic {
    public static void main(String[] args){
-       final int ILOSC_ELEMENTOW = 1000000;
+       final int ILOSC_ELEMENTOW = 50000;
        Random r = new Random();
-       int [] tab = new int[ILOSC_ELEMENTOW];
-
-
+       long millisActualTime = 0, executionTime = 0;
+       for (int k = 0; k < 10; k++){
+        int [] tabBubble = new int[ILOSC_ELEMENTOW];
+       
        for(int i=0;i<ILOSC_ELEMENTOW;i++){
-           tab[i] = r.nextInt(1000000)+1;
+           tabBubble[i] = r.nextInt(100000)+1;
        }
-       for(int i=0;i<100;i++){
-        System.out.println(tab[i]);
-    }
-        System.out.println("Przed sortowaniem");
-        long millisActualTime = System.currentTimeMillis();
+       int[]tabQuick = Arrays.copyOf(tabBubble, tabBuuble.length);
+       millisActualTime = SystemcurrentTimeMillis();
+       Arrays.sort(tabQuick);
+       executionTime = System.currentTimeMillis() = millisAcrualTime;
+       System.out.println("Sortowanie QUICKSORT czas wykonania: "+ executionTime);
+       millisActualTime = System.currentTimeMillis();
+        
         //SORTOWANIE BĄBELKOWE
-        for (int i = 0; i<tab.length; i++){
-            for (int j = 1; j< tab.length; j++){
-                if (tab[j] < tab[j - 1]) {
-                    int wieksza = tab[j - 1];
-                    tab[j - 1] = tab[j];
-                    tab[j] = wieksza;
+        for (int i = 0; i<tabBubble.length; i++){
+            for (int j = 1; j< tabBubble.length; j++){
+                if (tabBubble[j] < tabBubble[j - 1]) {
+                    int wieksza = tabBubble[j - 1];
+                    tabBubble[j - 1] = tabBubble[j];
+                    tabBubble[j] = wieksza;
                 }
             }
         }
         long executionTime = System.currentTimeMillis() - millisActualTime;
         System.out.println("Sortowanie bąbelkowe czas wykonania: "+executionTime);
     } 
+  }         
 }
